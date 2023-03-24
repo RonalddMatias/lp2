23be45339b4cbc72e1d2d5f00d31b7de75db5fdf
RonalddMatias <ronaldd.feliph.matias.costa@ccc.ufcg.edu.br>
2023-03-23 22:25:26 -0300

    atualizações lab01

--- lab01/src/Monotona.java
+++ lab01/src/Monotona.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 public class Monotona {
+	
+	/**
+	 * Laboratório de Programação 2 - Lab 1
+	 * 
+	 * Ronaldd Feliph Matias Costa - 122110574
+	 */
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
--- lab01/src/resultadoProvas.java
+++ lab01/src/resultadoProvas.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class resultadoProvas {
+	
+	/**
+	 * Laboratório de Programação 2 - Lab 1
+	 * 
+	 * Ronaldd Feliph Matias Costa - 122110574
+	 */
 
 	public static void main(String[] args) {
 		
